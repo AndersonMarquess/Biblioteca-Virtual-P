1435877e8ee81f32efde4f23558202ab3f9e33a1
AndersonMarquess <andersongabriel96.ag@gmail.com>
2019-04-15 17:14:12 -0300

    Adicionado HttpBasic para permitir testes com BasicAuth

--- src/main/java/com/andersonmarques/bvp/config/SegurancaConfig.java
+++ src/main/java/com/andersonmarques/bvp/config/SegurancaConfig.java
@@ -29,22 +29,24 @@ public class SegurancaConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-	    http.csrf().disable()
-		    	.exceptionHandling()
-		    	//Ponto de entrada para autenticação, se o request não for autenticado, 
-		    	//será devolvido uma resposta com status code 401 UNAUTHORIZED (O padrão é uma pagina html).
-		    	.authenticationEntryPoint(restAuthenticationEntryPoint)
+	    http.httpBasic()
+	    	.and()
+				// Se nenhum ponto de entrada for definido ao tentar fazer login com as
+				// credênciais inválidas, uma página html é retornada.
+	    		// Com a definição será retornado 401 Unauthorized.
+	    		.exceptionHandling()
+	    		.authenticationEntryPoint(restAuthenticationEntryPoint)
 	    	.and()
 			    .authorizeRequests()
 			    .antMatchers(AUTHENTICATED_ENDPOINTS).authenticated()
 			    .antMatchers(ADMIN_ENDPOINTS).hasRole("ADMIN")
 		    .and()
 			    .formLogin()
-			    //Success Handler personalizado para retornar com status code 200 (padrão é 301)
+			    //SuccessHandler personalizado para retornar com status code 200 (padrão é 301)
 			    .successHandler(mySuccessHandler)
 			    .failureHandler(new SimpleUrlAuthenticationFailureHandler())
 		    .and()
-		    .logout();
+		    	.csrf().disable();
 	}
 
 	/**
