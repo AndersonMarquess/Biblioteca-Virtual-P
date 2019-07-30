366bab0371878061506a22b68d70c8dc51c83a01
unknown <andersongabriel96.ag@gmail.com>
2019-07-30 18:47:30 -0300

    Adicionado funcionalidade de cadastrar livro

--- front-end/biblioteca-virtual/src/app/livros/form-novo-livro/form-novo-livro.component.ts
+++ front-end/biblioteca-virtual/src/app/livros/form-novo-livro/form-novo-livro.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Title } from '@angular/platform-browser';
+import { Router } from '@angular/router';
 import { Livro } from 'src/app/compartilhados/models/livro';
+import { UsuariosService } from 'src/app/usuarios/usuarios.service';
+import { LivrosService } from '../livros.service';
 
 @Component({
 	selector: 'bvp-form-novo-livro',
@@ -11,9 +15,11 @@ export class FormNovoLivroComponent implements OnInit {
 	formNovoLivro: FormGroup;
 	tituloPagina = "Compartilhar livro";
 
-	constructor(private formBuilder: FormBuilder) { }
+	constructor(private formBuilder: FormBuilder, private livrosService: LivrosService,
+		private title: Title, private router: Router, private usuarioService: UsuariosService) { }
 
 	ngOnInit(): void {
+		this.title.setTitle(this.tituloPagina);
 		this.formNovoLivro = this.formBuilder.group({
 			isbn: ['', [
 				Validators.required,
@@ -40,6 +46,25 @@ export class FormNovoLivroComponent implements OnInit {
 
 	montarECompartilharLivro(): void {
 		const livro = this.formNovoLivro.getRawValue() as Livro;
-		console.log(`Montou o livro: ${livro.isbn} ${livro.titulo} ${livro.categorias}`);
+		this.adicionarIdDoDono(livro);
+		livro.categorias = [{ id: null, nome: this.formNovoLivro.get("categorias").value }];
+		console.log(`Montou o livro ${JSON.stringify(livro)}`);
+
+		this.livrosService.cadastrar(livro)
+			.subscribe(
+				() => {
+					console.log("Cadastrado com sucesso");
+					this.router.navigate(['/livros', 'all']);
+				},
+				() => console.log("Erro ao tentar salvar livro")
+			);
+	}
+
+	private adicionarIdDoDono(livro: Livro): void {
+		this.usuarioService.getUsuarioLogado()
+			.subscribe(userResp => {
+				if (userResp != null)
+					livro.idDonoLivro = userResp.id;
+			});
 	}
 }
--- front-end/biblioteca-virtual/src/app/livros/livros.service.ts
+++ front-end/biblioteca-virtual/src/app/livros/livros.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Livro } from '../compartilhados/models/livro';
 
 const API_URL = "http://localhost:8080";
 
@@ -9,13 +10,18 @@ export class LivrosService {
 
 	constructor(private httpClient: HttpClient) { }
 
-	buscarTodosOsLivros(): Observable<any> {
+	buscarTodosOsLivros(): Observable<Array<Livro>> {
 		const endereco = API_URL + "/v1/livro/all";
-		return this.httpClient.get(endereco);
+		return this.httpClient.get<Array<Livro>>(endereco);
 	}
 
 	removerLivroComId(idLivro: string): Observable<any> {
 		const endereco = API_URL + "/v1/livro/" + idLivro;
 		return this.httpClient.delete(endereco);
+	}
+
+	cadastrar(livro: Livro): Observable<Livro> {
+		const endereco = API_URL + "/v1/livro";
+		return this.httpClient.post<Livro>(endereco, livro);
 	}
 }
